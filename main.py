ff4cdc919017da4f0b77029b29a7ca938c18fe6b
Nikita Sokolov <91872420+0kpts@users.noreply.github.com>
2022-02-14 08:33:41 +0000

    9.12

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-for i in range(1, 31):
-  if i%3==0:
-    print(i)
+n = int(input('Your number:'))
+if n <= 99:
+  print('2 digit word')
+if n >= 100:
+  print('3 digit word')
+if n % 2 == 0:
+  print('even')
+else:
+  print('odd')
+if n > 0:
+  print('pozitive')
+else:
+  print('negative')
